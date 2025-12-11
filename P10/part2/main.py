7dc103351f625dad312cbec1e0d4a6546df842bb
Gary Low <garylow2001@gmail.com>
2025-12-12 01:26:53 +0800

    solve p10 part2

--- P10/part2/main.py
+++ P10/part2/main.py
@@ -1,47 +1,47 @@
 # Example run: # python3 main.py < test.in > test.out
 from collections import deque
+import z3
 
 
 def parse_final(final_str):
-    # convert to bitmap for example [.##.] -> 0b0110 = 6
-    final_str = final_str[1:-1]
-    bitmap = 0b0
-    for i, ch in enumerate(final_str):
-        if ch == "#":
-            bitmap |= 1 << i
-    return bitmap
+    # convert to list of joltages e.g. {3,5,4,7} -> [3,5,4,7]
+    final_str = final_str[1:-1].split(",")
+    return list(map(int, final_str))
 
 
 def parse_button(button_str):
-    # convert to bitmask to xor later, for example "(1,3)" -> 0b1010 = 10
+    # convert to list of indexes to increment the counter e.g. "(1,3)" -> [1,3]
     ints_in_str = button_str[1:-1].split(",")
-    bitmask = 0b0
-    for num_str in ints_in_str:
-        num = int(num_str)
-        bitmask |= 1 << num  # convert to 0-based index
-    return bitmask
+    return list(map(int, ints_in_str))
+
+
+def increment_state(state, button):
+    # returns new tuple (immutable) state after applying button
+    new_state = list(state)
+    for index in button:
+        new_state[index] += 1
+    return tuple(new_state)
 
 
 def solve(final, buttons):
-    # final and buttons are in binary
-    initial = 0b0
-    queue = deque()
-    visited = set()
-    for b in buttons:
-        next_step = initial ^ b
-        visited.add(next_step)
-        queue.append((next_step, 1))  # (state, num_steps)
-    while queue:
-        state, num_steps = queue.popleft()
-        if state == final:
-            return num_steps
-        for b in buttons:
-            next_step = state ^ b
-            if next_step not in visited:
-                visited.add(next_step)
-                queue.append((next_step, num_steps + 1))
-    print("cant be solved")
-    return -1  # impossible to reach, explored all states already
+    # what if we represent final and buttons as matrices and find RREF
+    # a1x1 + a2x2 + ... + anxn = final
+    # xi is the button vector, ai is the number of times we press button i
+    # sum of a1 + a2 + ... + an = min steps -> {a1, a2, ..., an} = b, the vector of counts
+    # X.b = F   where X is matrix of buttons, b is vector of counts, F is final state
+    solver = z3.Optimize()
+    vars = z3.Ints(" ".join([f"x{i}" for i in range(len(buttons))]))
+    for v in vars:
+        solver.add(v >= 0)  # non-negative
+    for i, joltage in enumerate(final):
+        equation = 0
+        for b, button in enumerate(buttons):
+            if i in button:
+                equation += vars[b]
+        solver.add(equation == joltage)
+    solver.minimize(z3.Sum(vars))
+    solver.check()
+    return solver.model().eval(z3.Sum(vars)).as_long()
 
 
 def main():
@@ -54,7 +54,7 @@ def main():
     processed_lines = [line.strip() for line in lines if line.strip()]
     for line in processed_lines:
         sections = line.split(" ")
-        final = sections[0]
+        final = sections[-1]
         final = parse_final(final)
         buttons = sections[1:-1]  # ignore last rating
         buttons = [parse_button(b) for b in buttons]
